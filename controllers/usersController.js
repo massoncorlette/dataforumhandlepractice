55bc193e476a487836d74f19cc090522a96bf2a4
massoncorlette <massoncorlette07@gmail.com>
2025-03-20 03:53:17 -0400

    updating chain validation methods for new user properties in controller, need to update the controller functions

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -27,14 +27,18 @@ const { body, validationResult } = require("express-validator");
 
 const alphaErr = "must only contain letters.";
 const lengthErr = "must be between 1 and 10 characters.";
+const emailErr = "must be email format";
 
 const validateUser = [
   body("firstName").trim()
-    .isAlpha().withMessage(`First name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
+    .isAlpha().withMessage(`First name ${alphaErr}`),
   body("lastName").trim()
-    .isAlpha().withMessage(`Last name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+    .isAlpha().withMessage(`Last name ${alphaErr}`),
+  body("email").trim()
+    .isEmail().withMessage(`Email ${emailErr}`),
+  body("age").trim()
+    .isInt({ min: 1, max: 120 }).withMessage('between 18 and 100'),
+
 ];
 
 
--- storages/usersStorage.js
+++ storages/usersStorage.js
@@ -6,9 +6,9 @@ class UsersStorage {
     this.id = 0;
   }
 
-  addUser({ firstName, lastName }) {
+  addUser({ firstName, lastName, age, email, bio }) {
     const id = this.id;
-    this.storage[id] = { id, firstName, lastName };
+    this.storage[id] = { id, firstName, lastName, age, email, bio };
     this.id++;
   }
 
@@ -20,8 +20,8 @@ class UsersStorage {
     return this.storage[id];
   }
 
-  updateUser(id, { firstName, lastName }) {
-    this.storage[id] = { id, firstName, lastName };
+  updateUser(id, { firstName, lastName, age, email, bio}) {
+    this.storage[id] = { id, firstName, lastName, age, email, bio };
   }
 
   deleteUser(id) {
